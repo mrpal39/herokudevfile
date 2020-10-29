e53fe1627049c22817ea9b591b3a9edf4749d5ac
mrpal39 <rp9545416@gmail.com>
2020-10-29 18:27:48 +0530

    make it fffgddgggggffffff

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dev.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -11,5 +11,5 @@ if __name__ == '__main__':
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-        
+
     execute_from_command_line(sys.argv)
