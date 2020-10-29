5683f02d13345621161885964b983cd034eb0d93
mrpal39 <rp9545416@gmail.com>
2020-10-29 18:19:27 +0530

    make it fffgddggffffff

--- dev/settings.py
+++ dev/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 
 STATIC_ROOT = (BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 MEDIA_ROOT = (BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
