1e0c8f1b115a5b846299ff3089401eed9dbd13f2
mrpal39 <rp9545416@gmail.com>
2020-10-30 09:06:37 +0530

    make it fffffccgddddfgggfffffgfffggggffffff

--- dev/settings.py
+++ dev/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = ["https://quiet-chamber-10055.herokuapp.com/"]
+ALLOWED_HOSTS = ['quiet-chamber-10055.herokuapp.com']
 
 
 # Application definition
